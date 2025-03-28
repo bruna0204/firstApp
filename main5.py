2e7cd136666840d3d9a06f75d5b1b50303e77cd2
bruna0204 <bruna02042008@gmail.com>
2025-03-28 09:56:14 -0300

    27/03

--- main5.py
+++ main5.py
@@ -19,31 +19,30 @@ def main(page: ft.Page):
         idade_atual = tempo_atual.year - data_informada.year
         meses_atual = tempo_atual.month
         dias_atual = tempo_atual.day
+        try:
 
-        if meses_atual < data_informada.month:
-            idade_atual = idade_atual - 1
-
-
-        elif meses_atual == data_informada.month:
-            if dias_atual < data_informada.day:
+            if meses_atual < data_informada.month:
                 idade_atual = idade_atual - 1
 
-        if idade_atual < 18:
-            txt_resultado.value = f' sua idade é {idade_atual} você é menor de idade'
-            page.update()
+
+            elif meses_atual == data_informada.month:
+                if dias_atual < data_informada.day:
+                    idade_atual = idade_atual - 1
+
+            if idade_atual < 18:
+                txt_resultado.value = f' sua idade é {idade_atual} você é menor de idade'
+                page.update()
 
 
+            elif idade_atual > 110 or idade_atual < 0 :
+                txt_resultado.value = f'Essa pessoa não existe'
+                page.update()
 
-
-
-
-        elif idade_atual > 110:
-            txt_resultado.value = f'Essa pessoa não existe'
-
-        else:
-            txt_resultado.value = f' sua idade é {idade_atual} você é maior de idade'
-            page.update()
-
+            else:
+                txt_resultado.value = f' sua idade é {idade_atual} você é maior de idade'
+                page.update()
+        except ValueError:
+             txt_resultado.value = 'digite apenas numeros'
 
 
 
